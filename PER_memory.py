6395d839750dd7125a5a5475eda192cbe34a5a11
Sankar_Madhavan_B <85505152+wittyviking007@users.noreply.github.com>
2024-04-21 12:26:24 +0530

    Update PER_memory.py

--- PER_memory.py
+++ PER_memory.py
@@ -176,6 +176,7 @@ class ReplayBuffer(object):
             s_, a_, ac_, r_, s2_, gamma_, flag_ = [], [], [], [], [], [], []
             for t in exp:
                 s, a, ac, r, s2, gamma, flag = t
+                print("s: ",s)
                 s_.append(s.clone())
                 a_.append(a.clone())
                 ac_.append(ac.clone())
@@ -184,7 +185,7 @@ class ReplayBuffer(object):
                 gamma_.append(gamma.clone())
                 flag_.append(flag)
         # stack along new axis
-            print("s_ : " ,s_.shape)
+            print("s_ : " ,len(s_)
             stt.append(torch.stack(s_))
             att.append(torch.stack(a_))
             actt.append(torch.stack(ac_))

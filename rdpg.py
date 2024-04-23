fa891697cf28f98e8863d27d06577d81825d39f4
Sankar_Madhavan_B <85505152+wittyviking007@users.noreply.github.com>
2024-04-23 23:01:41 +0530

    Update rdpg.py

--- rdpg.py
+++ rdpg.py
@@ -165,6 +165,7 @@ class RDPG(object):
                             
                         state0_cuda = to_tensor(np.array([state0])).cuda()
                         # state0_cuda = to_tensor(state0).cuda()
+                        print("state0 cuda: ",state0_cuda)
                         action, epsilon = self.agent.select_action(state0_cuda)
                         action_bc, next_state, reward, done, infos = self.env.step(np.argmax(action))
                 ######################################################################
